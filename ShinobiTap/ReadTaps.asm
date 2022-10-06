dde38c37e19f518681b2155c2e9686a640317a86
Danjovic <Danjovic@users.noreply.github.com>
2022-10-06 19:53:43 -0300

    added joystick type to return byte in B register

--- ShinobiTap/ReadTaps.asm
+++ ShinobiTap/ReadTaps.asm
@@ -6,8 +6,13 @@ org 09000h
 CHECKPLYRS:
 ; Return the number of players connected
 ; [E] None                     
-; [R] C Tap connected info 
-;     B number of players connected
+; [R] C Tap connected info ( 0  0  0  0  a1 a0 b1 b0)
+;     B number of players connected and interface type
+;  7   6   5   4   3   2   1   0
+;  │   │   │   │   └───┴───┴───┴── Number of joystick ports available (2, 5 or 8)
+;  │   │   └───┴────────────────── Tap type pluged in port 1 
+;  └───┴────────────────────────── Tap type pluged in port 2 
+
 	call CHECKTAPS
 	ld b,0
 	ld hl,TBPLRS
@@ -16,25 +21,30 @@ CHECKPLYRS:
 	ret
 	
 TBPLRS: ; unknown count as 1
-   db  8,8,5,5,8,8,5,5,5,5,2,2,5,5,2,2
+	db 0A8h,068h,0E5h,025h,098h,058h,0D5h,015h,0B5h,075h,0F2h,032h,085h,045h,0C2h,002h
+;   db  8,8,5,5,8,8,5,5,5,5,2,2,5,5,2,2
 
-;   a1 a0 b1 b0   Tap a, Tapb   Count
-;   0  0  0  0     4      4       8
-;   0  0  0  1     4      4       8
-;   0  0  1  0     4      1       5
-;   0  0  1  1     4      1       5
-;   0  1  0  0     4      4       8
-;   0  1  0  1     4      4       8
-;   0  1  1  0     4      1       5
-;   0  1  1  1     4      1       5
-;   1  0  0  0     1      4       5
-;   1  0  0  1     1      4       5
-;   1  0  1  0     1      1       2
-;   1  0  1  1     1      1       2
-;   1  1  0  0     1      4       5
-;   1  1  0  1     1      4       5
-;   1  1  1  0     1      1       2
-;   1  1  1  1     1      1       2
+;  Input: C          output B   #players  Table    
+;  P.Ty.#  P.Ty.#   P.Ty  P.Ty  B.#+A.#   bin
+;  A.ST.4  B.ST.4   B.10  A.10    4+4     1010 <<4  + 8  = 0A8h
+;  A.ST.4  B.NJ.4   B.01  A.10    4+4     0110 <<4  + 8  = 068h
+;  A.ST.4  B.UK.1   B.11  A.10    1+4     1110 <<4  + 5  = 0E5h
+;  A.ST.4  B.JO.1   B.00  A.10    1+4     0010 <<4  + 5  = 025h
+;  A.NJ.4  B.ST.4   B.10  A.01    4+4     1001 <<4  + 8  = 098h
+;  A.NJ.4  B.NJ.4   B.01  A.01    4+4     0101 <<4  + 8  = 058h
+;  A.NJ.4  B.UK.1   B.11  A.01    1+4     1101 <<4  + 5  = 0D5h
+;  A.NJ.4  B.JO.1   B.00  A.01    1+4     0001 <<4  + 5  = 015h
+;  A.UK.1  B.ST.4   B.10  A.11    4+1     1011 <<4  + 5  = 0B5h
+;  A.UK.1  B.NJ.4   B.01  A.11    4+1     0111 <<4  + 5  = 075h
+;  A.UK.1  B.UK.1   B.11  A.11    1+1     1111 <<4  + 2  = 0F2h
+;  A.UK.1  B.JO.1   B.00  A.11    1+1     0011 <<4  + 2  = 032h
+;  A.JO.1  B.ST.4   B.10  A.00    4+1     1000 <<4  + 5  = 085h
+;  A.JO.1  B.NJ.4   B.01  A.00    4+1     0100 <<4  + 5  = 045h
+;  A.JO.1  B.UK.1   B.11  A.00    1+1     1100 <<4  + 2  = 0C2h
+;  A.JO.1  B.JO.1   B.00  A.00    1+1     0000 <<4  + 2  = 002h
+;  
+;  
+
 
 
 CHECKTAPS:
